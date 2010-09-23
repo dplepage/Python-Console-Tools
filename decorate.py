5238f90a5b7c5482204fb243111456a6456a47df
Daniel Lepage <dplepage@gmail.com>
2010-09-23 14:01:06 -0400

    Fixed example2 and moved its core to decorate
    I resolved the scoping problem by sidestepping it, using staticmethods instead of instancemethods, and moved the @subcommand implementation from example2.py to decorate.py. example2.py now demonstrates how a program would use this sort of tool.

--- decorate.py
+++ decorate.py
@@ -1,5 +1,6 @@
 import sys
 import warnings
+import plac
 
 class MultipleMainWarning(Warning):
     pass
@@ -9,10 +10,24 @@ def main(fn):
         if hasattr(main,'_has_target'):
             warnings.warn(MultipleMainWarning("Too many mains!"))
         main._has_target=True
-        import plac
         def call(fn, *args, **kwargs):
             try:
                 plac.call(fn,*args,**kwargs)
             except Exception,e:
                 print "{0}: {1}: {2}".format(sys.argv[0], type(e).__name__, e)
         call(fn)
+
+
+subs = []
+def subcommand(fn):
+    subs.append(fn)
+    return fn
+
+def run_subcommands():
+    class Foo(object):
+        commands = [f.__name__ for f in subs]
+        def _dummy(): pass
+    
+    for cmd in subs:
+        setattr(Foo, cmd.__name__, staticmethod(cmd))
+    plac.Interpreter.call(Foo)
--- example2.py
+++ example2.py
@@ -1,25 +1,12 @@
-import plac
-
-subs = []
-def subcommand(fn):
-    subs.append(fn)
-    return fn
+from decorate import subcommand, run_subcommands
 
 @subcommand
 def cmd1():
     print "hi!"
 
 @subcommand
-def cmd2():
-    print "ho!"
+def cmd2(arg="arg!"):
+    print "Called cmd2 with arg: {0}",format(arg)
 
-
-class Foo(object):
-    commands = [f.__name__ for f in subs]
-    def _dummy(): pass
-    
-for cmd in subs:
-    setattr(Foo, cmd.__name__, lambda self, *args, **kwargs: globals()[cmd.__name__](*args,**kwargs))
-    
 if __name__ == '__main__':
-    plac.Interpreter.call(Foo)
+    run_subcommands()
